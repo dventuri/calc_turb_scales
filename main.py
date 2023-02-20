c0bc7277898c65df58593ce17396f713295e0182
Diego N. Venturi <diegoventuri@gmail.com>
2023-02-20 12:20:45 -0300

    changing calculation

--- main.py
+++ main.py
@@ -20,26 +20,22 @@ def Taylor(nu, U, L):
     eta = comp_caract_Kolmogorov(nu, U, L)
     Re_L = Reynolds(nu, U, L)
     gamma = eta*7.0*np.power(Re_L, 1.0/4.0)
-    
+
     return gamma
 
 
 if __name__ == "__main__":
 
-    nu = 1.7894e-05/1.225
-    U = 1.5
-    D = 0.12
+    nu_air = 1.8448e-5/1.1843
+    nu_water = 8.9002e-4/997.05
 
-    Re = Reynolds(nu, U, D)
-    print(Re)
+    L = 0.55
+    L_air = L*0.75
+    L_water = L - L_air
 
-    #Kolmogorov
-    eta = comp_caract_Kolmogorov(nu, U, D)
-    print(eta)
-
-    #escala de Taylor
-    gamma = Taylor(nu, U, D)
-    print(gamma)
+    U = 20.14
+    U_air = U*(L**2)/(L_air**2)
+    U_water = U_air/3
 
     # "Para se ter uma banda inercial bem definida, é necessário que
     #  L/eta > 1000. Então o microcomprimento de Taylor deve ser pelo
@@ -48,5 +44,33 @@ if __name__ == "__main__":
     #  comprimento característico de Kolmogorov e abaixo do comprimento
     #  integral."
 
-    print(D/eta)
-    print(gamma/eta)
+    ### AIR
+    Re = Reynolds(nu_air, U_air, L_air)
+    print("Re = ", Re)
+
+    #Kolmogorov
+    eta = comp_caract_Kolmogorov(nu_air, U_air, L_air)
+    print("eta = ", eta)
+
+    #escala de Taylor
+    gamma = Taylor(nu_air, U_air, L_air)
+    print("gamma = ", gamma)
+
+    print("L/eta = ", L_air/eta)
+    print("gamma/eta = ", gamma/eta)
+    print("")
+
+    ### WATER
+    Re = Reynolds(nu_water, U_water, L_water)
+    print("Re = ", Re)
+
+    #Kolmogorov
+    eta = comp_caract_Kolmogorov(nu_water, U_water, L_water)
+    print("eta = ", eta)
+
+    #escala de Taylor
+    gamma = Taylor(nu_water, U_water, L_water)
+    print("gamma = ", gamma)
+
+    print("L/eta = ", L_air/eta)
+    print("gamma/eta = ", gamma/eta)
